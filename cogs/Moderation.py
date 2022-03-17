e1bb616ebc930a1b259309d33ec58bccd48bd1c4
Luca <83220423+kiisuhh@users.noreply.github.com>
2022-03-17 23:38:03 +0100

    Update Moderation.py

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -10,24 +10,48 @@ load_dotenv()
 
 class Moderation(commands.Cog):
     def __init__(self, bot):
-        bot: commands.Bot = bot
+        self.bot: commands.Bot = bot
 
     @commands.command()
     async def warn(self, ctx):
-        await ctx.reply("works")
+        pass
 
     @commands.command()
     async def kick(self, ctx, user = None, reason: str = None):
         modrole = get(ctx.guild.roles, id=int(os.getenv("MODID")))
 
-        if ctx.author.has_role(modrole):
+        if modrole in ctx.author.roles:
             if user is not None:
+                try:
+                    if ctx.message.mentions[0] is not None:
+                        user = ctx.message.mentions[0]
+                except Exception as e:
+                    try:
+                        if self.bot.get_user(int(user)) is not None:
+                            user = self.bot.get_user(int(user))
+                        else:
+                            await ctx.reply(embed=discord.Embed(title="Twilight Dawn Moderation",
+                                                                description="User nicht gefunden."))
+                            return True
+                    except Exception as e2:
+                        await ctx.reply(embed=discord.Embed(title="Twilight Dawn Moderation",
+                                                            description="User nicht gefunden."))
+                        return True
+
+
                 if reason is not None:
                     await user.kick(reason=reason)
                 else:
-                    await user.kick()
+                    await ctx.guild.get_member(user.id).kick()
+                    await ctx.reply(embed=discord.Embed(
+                        title="Twilight Dawn Moderation",
+                        description=f"{user.mention} wurde vom Server gekickt."
+                    ))
             else:
-                ctx.reply(embed=discord.Embed(title="Twilight Dawn Moderation", description="Bitte gib einen User an."))
+                await ctx.reply(embed=discord.Embed(title="Twilight Dawn Moderation", description="Bitte gib einen User an."))
+
+        else:
+            await ctx.reply(embed=discord.Embed(title="Twilight Dawn Moderation", description="Dazu hast du keine Rechte."))
 
 
     @commands.command()
